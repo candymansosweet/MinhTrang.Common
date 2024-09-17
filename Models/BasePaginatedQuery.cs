04f0a8ed0b5fb7e440b2b518bd8bb01235a7b82d
Trung Lê Hoàng <trunglh@pmr.vn>
2024-09-17 21:17:31 +0700

    add query

---
+++ Models/BasePaginatedQuery.cs
@@ -0,0 +1,8 @@
+﻿public class BasePaginatedQuery
+{
+    public string Keyword { get; set; } = string.Empty;
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 20;
+    public string SortField { get; init; } = string.Empty;
+    public string SortOrder { get; init; } = string.Empty;
+}
