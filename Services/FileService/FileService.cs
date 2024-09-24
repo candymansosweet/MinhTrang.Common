cfe418816626ee248fefa898eea11f2aead25f88
Trung Lê Hoàng <trunglh@pmr.vn>
2024-09-24 18:51:06 +0700

    s

---
+++ Services/FileService/FileService.cs
@@ -0,0 +1,47 @@
+﻿using Common.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Services.FileService
+{
+    public class FileService : IFileService
+    {
+        public async Task DeleteFileByPath(string path)
+        {
+            string directory = Path.GetDirectoryName(path) ?? throw new AppException(ExceptionCode.Invalidate, "Đường dẫn không được để trống");
+            if (!Directory.Exists(directory))
+            {
+                throw new AppException(ExceptionCode.Notfound, "Không tìm thấy folder " + directory);
+            }
+            File.Delete(path);
+        }
+
+        public async Task<byte[]> DownloadFileByPath(string path)
+        {
+            string directory = Path.GetDirectoryName(path) ?? throw new AppException(ExceptionCode.Invalidate, "Đường dẫn không được để trống");
+            if (!Directory.Exists(directory))
+            {
+                throw new AppException(ExceptionCode.Notfound, "Không tìm thấy folder " + directory);
+            }
+            return await File.ReadAllBytesAsync(path);
+        }
+
+        public async Task UploadFileByPath(string path, IFormFile formFile)
+        {
+            string directory = Path.GetDirectoryName(path) ?? throw new AppException(ExceptionCode.Invalidate, "Đường dẫn không được để trống");
+            if (!Directory.Exists(directory))
+            {
+                throw new AppException(ExceptionCode.Notfound, "Không tìm thấy folder " + directory);
+            }
+            // Sử dụng FileStream để lưu file vào đường dẫn
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await formFile.CopyToAsync(stream);
+            }
+        }
+    }
+}
---
+++ Services/FileService/IFileService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Services.FileService
+{
+    public interface IFileService
+    {
+        public Task UploadFileByPath(string path, IFormFile formFile);
+        public Task DeleteFileByPath(string path);
+        public Task<byte[]> DownloadFileByPath(string path);
+    }
+}
