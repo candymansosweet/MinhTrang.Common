957754e7ae9c104081cec06daee3f86d3d32479a
Trung Lê Hoàng <trunglh@pmr.vn>
2025-04-10 13:52:58 +0700

    e

---
+++ Constants/RoleValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Constants
+{
+    public static class RoleValue
+    {
+        public enum ROLE
+        {
+            Admin = 0,
+            Staff = 5
+        }
+    }
+}
